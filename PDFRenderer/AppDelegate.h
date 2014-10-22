a026e62ef2461efeb7d3651426929d449b6c2c3b
Bruno da Luz <brunolabx@gmail.com>
2014-10-22 20:40:27 -0200

    Initial Commit

---
+++ PDFRenderer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFRenderer
+//
+//  Created by Bruno on 10/22/14.
+//  Copyright (c) 2014 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDFRenderer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDFRenderer
+//
+//  Created by Bruno on 10/22/14.
+//  Copyright (c) 2014 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
